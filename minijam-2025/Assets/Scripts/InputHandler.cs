8b6a67a2d583fc9ea6c1cacd8c8c0fd9e0e83128
willow-flame <willowflameproductions@gmail.com>
2025-02-16 11:18:27 +0000

    Working Click Event
    Doesn't always trigger though

--- minijam-2025/Assets/Scripts/InputHandler.cs
+++ minijam-2025/Assets/Scripts/InputHandler.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class InputHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Referencing the main camera
+    private Camera _maincamera;
+
+    private void Awake()
     {
-        
+        _maincamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClick(InputAction.CallbackContext context)
     {
-        
+        //If not clicked
+        if (!context.started) return;
+
+        var rayHit = Physics2D.GetRayIntersection(_maincamera.ScreenPointToRay(pos:(Vector2)Mouse.current.position.ReadValue()));
+        //Checks if clicking object with a colllider
+        if (!rayHit.collider) return;
+
+        Debug.Log(rayHit.collider.gameObject.name);
     }
 }
